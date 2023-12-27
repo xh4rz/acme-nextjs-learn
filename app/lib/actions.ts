abdcf45fc6f36eabd95c452d2b262fefad6c2ccc
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2023-12-27 16:41:35 -0500

    13-Handling Errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -26,10 +26,16 @@ export async function createInvoice(formData: FormData) {
 
 	const date = new Date().toISOString().split('T')[0];
 
-	await sql`
+	try {
+		await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+	} catch (error) {
+		return {
+			message: 'Database Error: Failed to Create Invoice.'
+		};
+	}
 
 	revalidatePath('/dashboard/invoices');
 
@@ -47,11 +53,15 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 	const amountInCents = amount * 100;
 
-	await sql`
+	try {
+		await sql`
         UPDATE invoices
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
         WHERE id = ${id}
     `;
+	} catch (error) {
+		return { message: 'Database Error: Failed to Update Invoice.' };
+	}
 
 	revalidatePath('/dashboard/invoices');
 
@@ -59,9 +69,14 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-	await sql`
+	try {
+		await sql`
         DELETE FROM invoices WHERE id = ${id}
     `;
+		revalidatePath('/dashboard/invoices');
 
-	revalidatePath('/dashboard/invoices');
+		return { message: 'Deleted Invoice.' };
+	} catch (error) {
+		return { message: 'Database Error: Failed to Delete Invoice.' };
+	}
 }
